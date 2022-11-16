2e2229c9bfee001ca1bbcb91a79b0eae7c8322a4
DanielWilliamFaessler <daniel.faesslerasmodi@gmail.com>
2022-11-16 16:57:30 +0100

    added 10 things

--- bildergallerie/src/index.js
+++ bildergallerie/src/index.js
@@ -11,39 +11,39 @@ root.render(
             <p className="legend">Legend 1</p>
         </div>
         <div>
-            <img src="assets/1.jpg" />
+            <img src="assets/2.jpg" />
             <p className="legend">Legend 2</p>
         </div>
         <div>
-            <img src="assets/1.jpg" />
+            <img src="assets/3.PNG" />
             <p className="legend">Legend 3</p>
         </div>
         <div>
-            <img src="assets/1.jpg" />
+            <img src="assets/4.PNG" />
             <p className="legend">Legend 3</p>
         </div>
         <div>
-            <img src="assets/1.jpg" />
+            <img src="assets/5.jpg" />
             <p className="legend">Legend 3</p>
         </div>
         <div>
-            <img src="assets/1.jpg" />
+            <img src="assets/6.jpg" />
             <p className="legend">Legend 3</p>
         </div>
         <div>
-            <img src="assets/1.jpg" />
+            <img src="assets/7.png" />
             <p className="legend">Legend 3</p>
         </div>
         <div>
-            <img src="assets/1.jpg" />
+            <img src="assets/8.jpg" />
             <p className="legend">Legend 3</p>
         </div>
         <div>
-            <img src="assets/1.jpg" />
+            <img src="assets/9.jpeg" />
             <p className="legend">Legend 3</p>
         </div>
         <div>
-            <img src="assets/1.jpg" />
+            <img src="assets/10.jpg" />
             <p className="legend">Legend 3</p>
         </div>
     </Carousel>
